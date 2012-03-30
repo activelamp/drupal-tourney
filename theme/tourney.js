1e55846cfacc404e51d62f42e0c5dfcb4484edbc
dragonwize <drupal@dragonwize.net>
2012-03-30 16:21:32 -0700

    Initial commit.

--- theme/tourney.js
+++ theme/tourney.js
@@ -9,8 +9,7 @@
 
         if (highlight) {
           // Highlight contestants
-          $('div.' + highlight).addClass('bracket-highlight')
-            .not(':last').closest('div.match-contestant').find('div.flow').addClass('bracket-highlight');
+          $('div.' + highlight).addClass('bracket-highlight');
         }
       },
       function() {
