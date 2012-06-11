f9fba28b9160e4a70aafdf3396e269dcdb6c1fa9
Jonathan Roberts <jon@activelamp.com>
2012-06-11 13:54:06 -0600

    really basic implementation of a new controller

---
+++ plugins/tourney_formats/BaseFormatControllers/SpecialElimination.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * @file
+ * Single elimination controller, new system.
+ */
+
+class SpecialEliminationController extends TourneyController implements TourneyControllerInterface {
+  public $slots;
+  public $contestants;
+  public $data;
+  public $structure;
+
+  public function __construct($contestants) {
+    $this->contestants = $contestants;  
+    $this->slots = pow(2, ceil(log($contestants, 2)));
+    $this->build();
+    $this->structure();
+  }
+
+  public function build() {
+    $match = 0;
+    $slots = $this->slots;
+    $round = 0;
+    while ( ($slots /= 2) >= 1 ) {
+      $this->data['rounds'][++$round] = array(
+          'title'   => "Round $round",
+          'id'      => "round-$round",
+      );
+      foreach ( range(1, $slots) as $n ) {
+        $this->data['matches'][++$match] = array(
+          'title'   => "Match $match",
+          'id'      => "match-$match",
+          'round'   => "round-$round",
+        );
+      }
+    }
+  }
+
+  public function structure() {
+    $structre = array();
+    foreach ( $this->data['rounds'] as $round ) {
+      $structure[$round['id']] = array(
+        'matches' => array(),
+      );
+    }
+    foreach ( $this->data['matches'] as $match ) {
+      $structure[$match['round']]['matches'][$match['id']] = $match;
+    }
+    $this->structure = $structure;
+  }
+
+  public function determineWinner($tournament) {
+
+  }
+
+  public function isFinished($tournament) {
+
+  }
+}
