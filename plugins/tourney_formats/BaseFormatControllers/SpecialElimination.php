f6a453e1009dc6b2a3a8270b1965523143e25ef9
Jonathan Roberts <jon@activelamp.com>
2012-06-11 14:06:22 -0600

    Added method calling from build()

--- plugins/tourney_formats/BaseFormatControllers/SpecialElimination.php
+++ plugins/tourney_formats/BaseFormatControllers/SpecialElimination.php
@@ -23,18 +23,37 @@ class SpecialEliminationController extends TourneyController implements TourneyC
     $slots = $this->slots;
     $round = 0;
     while ( ($slots /= 2) >= 1 ) {
-      $this->data['rounds'][++$round] = array(
-          'title'   => "Round $round",
-          'id'      => "round-$round",
-      );
+      $this->data['rounds'][++$round] = 
+        $this->buildRound(array('id' => $round));
+
       foreach ( range(1, $slots) as $n ) {
-        $this->data['matches'][++$match] = array(
-          'title'   => "Match $match",
-          'id'      => "match-$match",
-          'round'   => "round-$round",
-        );
+        $this->data['matches'][++$match] = 
+          $this->buildMatch(array(
+            'id' => $match,
+            'round' => "round-$round",
+          ));
       }
     }
+  }
+
+  public function buildRound($data) {
+    $round = array(
+      'title' => 'Round ' . $data['id'],
+      'id'    => 'round-' . $data['id'],
+    );
+    unset($data['id']);
+    //$round += $data;
+    return $round;
+  }
+
+  public function buildMatch($data) {
+    $match = array(
+      'title'   => "Match " . $data['id'],
+      'id'      => "match-" . $data['id'],
+    );
+    unset($data['id']);
+    $match += $data;
+    return $match;
   }
 
   public function structure() {
