899483612c8695affb227698142cc349c07d9f91
Tom Friedhof <tom@activelamp.com>
2012-06-25 16:38:27 -0700

    removing deleted files

--- theme/tourney-tournament-tree.tpl.php
+++
@@ -1,7 +0,0 @@
-<?php
-drupal_add_css(drupal_get_path('module', 'tourney') . '/theme/tourney.css');
-?>
-
-<div class="tournament-bracket tournament-tree">
-  <?php echo theme('tourney_tournament_tree_node', array('node' => $structure['tree'])); ?>
-</div>
