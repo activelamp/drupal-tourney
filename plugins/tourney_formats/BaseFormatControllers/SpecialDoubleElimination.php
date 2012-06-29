b1cc88102222c6a1597f2ce2c6f61a155e9984a1
Tom Friedhof <tom@activelamp.com>
2012-06-28 18:37:55 -0700

    created SpecialDoubleElimination, also created a setting for truncating usernames, this fieldset can be used to eventually choose what entities can compete, and which field to use as the contestant name.

---
+++ plugins/tourney_formats/BaseFormatControllers/SpecialDoubleElimination.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @file
+ * Single elimination controller, new system.
+ */
+
+/**
+ * A class defining how matches are created, and rendered for this style
+ * tournament.
+ */
+class SpecialDoubleElimination extends SingleEliminationController {
+  
+}
