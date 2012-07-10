ebe6dcf277580ecc23df8450fd491a7a0bcc03e2
Tom Friedhof <tom@activelamp.com>
2012-07-09 19:17:14 -0700

    adding the start of the special round robin

---
+++ plugins/tourney_formats/BaseFormatControllers/SpecialRoundRobin.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @file
+ * A Round Robin class for tournaments.
+ */
+
+/**
+ * A class defining how matches are created for this style tournament.
+ */
+
+class SpecialRoundRobinController extends TourneyController {
+
+
+}
