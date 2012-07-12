baff8e8ecbeead7294bfa686a487d5f44695398c
Tom Friedhof <tom@activelamp.com>
2012-07-12 10:47:36 -0700

    finished up round robin standings

---
+++ plugins/tourney_formats/round_robin/theme/tourney-roundrobin-standings.tpl.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @file tourney-roundrobin-standings.tpl.php
+ * 
+ * Round robin standings table.
+ *
+ * @ingroup tourney_templates
+ */
+?>
+<h2>Round Robin Standings</h2>
+<div id="standings">
+  <?php print $standings; ?>
+</div>
