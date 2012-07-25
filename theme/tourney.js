9c3444bc566d89177df3b1abf17c09b80c04666d
Tom Friedhof <tom@activelamp.com>
2012-07-25 13:20:17 -0700

    removing race condition when hitting both win buttons

--- theme/tourney.js
+++ theme/tourney.js
@@ -1,11 +1,21 @@
 (function($) {
-  Drupal.behaviors.setWidth = {
+  Drupal.behaviors.tourneySetWidth = {
     attach: function(context, settings) {
       $(".tourney", context).each(function(){
         $rounds = $(".tourney-single .round, .bracket-top .round, .bracket-champion .round:not(:empty)", context);
         rw = $($rounds[0]).css('width');
         if ( !rw ) return;
         $('.tourney-inner', this).css('width', (parseInt(rw)+parseInt(rw)*$rounds.length)+'px');
+      });
+    }
+  };
+  Drupal.behaviors.tourneyDisableWin = {
+    attach: function(context, settings) {
+      $(".win-button", context).click(function() {
+        $(".win-button", context).each(function() {
+          $(this).hide();
+          $('<div class="ajax-progress"><span class="throbber"></span></div>').insertAfter(this);
+        })
       });
     }
   };
