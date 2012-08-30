5f337abf7e1a26d4f452dd4b931dc1ff2aaa55ee
Jonathan Roberts <jon@activelamp.com>
2012-08-30 17:06:11 -0600

    drupal coding standards.

--- plugins/tourney_formats/single_elim/theme/tournament-tree-node.js
+++ plugins/tourney_formats/single_elim/theme/tournament-tree-node.js
@@ -17,27 +17,27 @@ Drupal.behaviors.tourneyFixConnectors = { attach: function(context, settings) {
     var $children = $this.children('.children').children('.tree-node');
     var $parent   = $this.children('.parent');
 
-    if ( $children.length == 0 ) return;
+    if ($children.length == 0) return;
 
     var $connectors = $('.connector .path', $children.children('.parent'));
 
     var $connectParent = $('.connector', $parent);
     var $connectBottom = $connectors.last();
     var $connectTop    = null;
-    if ( $connectors.length > 1 ) 
+    if ($connectors.length > 1) 
       $connectTop      = $connectors.first(); 
 
     var connectTarget  = $connectParent.offset().top + $connectParent.height();
     var connectOffset  = $connectBottom.offset().top - connectTarget;
 
-    if ( connectOffset <= 0 ) return;    
+    if (connectOffset <= 0) return;    
 
-    if ( connectOffset ) {
+    if (connectOffset) {
       $connectBottom.css({
         height: $connectBottom.height() + connectOffset,
         top: -connectOffset
       });
-      if ( $connectTop )
+      if ($connectTop)
         $connectTop.css('height', $connectTop.height() - connectOffset);
     }
   });
@@ -58,7 +58,7 @@ Drupal.behaviors.tourneyHighlight = { attach: function(context, settings) {
     $('.contestant' + eid).closest('.match').addClass('tourney-highlight');
     $('.connector' + eid).each(function(){
       $parent = $(this).closest('.parent').closest('.children').closest('.tree-node');
-      if ( $(this).hasClass('winner-bottom') ) $('.connector.to-children', $(this).parent()).addClass('winner-bottom');
+      if ($(this).hasClass('winner-bottom')) $('.connector.to-children', $(this).parent()).addClass('winner-bottom');
       $('.connector.to-children', $parent.children('.parent')).addClass('tourney-highlight');
     });
   }, function() {
