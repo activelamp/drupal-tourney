32c5fda600eb25ebd48ea8e42938a63a02ee5022
Jonathan Roberts <jon@activelamp.com>
2012-08-31 13:08:29 -0600

    find() back in, replacing calculateNextPosition
    pathing, seeds, and skipping byes all reimplemented

--- theme/tourney-match-render.tpl.php
+++ theme/tourney-match-render.tpl.php
@@ -5,12 +5,27 @@
  *
  * @ingroup tourney_templates
  */
+$letters = array('', 
+  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+  'AA', 'AB', 'AC', 'AD', 'AE', 'AF', 'AG', 'AH', 'AI', 'AJ', 'AK', 'AL', 'AM', 'AN', 'AO', 'AP', 'AQ', 'AR', 'AS', 'AT', 'AU', 'AV', 'AW', 'AX', 'AY', 'AZ',
+  'BA', 'BB', 'BC', 'BD', 'BE', 'BF', 'BG', 'BH', 'BI', 'BJ', 'BK', 'BL', 'BM', 'BN', 'BO', 'BP', 'BQ', 'BR', 'BS', 'BT', 'BU', 'BV', 'BW', 'BX', 'BY', 'BZ',
+  );
 ?>
-<div class="match">
+<div class="match <?php if ( isset($match['bye']) && $match['bye'] ) print 'bye'; ?>">
   <?php if ($has_children): ?>
     <div class="connector to-children <?php print $children_classes; ?>"></div>
   <?php endif; ?>
-  <?php /* <div class="contestant tourney-contestant contestant-1"><?php print $match['previousMatches'][1] . ' - ' . $match['id'] . ' - ' . $match['nextMatch']['winner']['id']?></div>
-  <div class="contestant tourney-contestant contestant-2"><?php print $match['previousMatches'][2] . ' - ' . $match['id'] . ' - ' . $match['nextMatch']['loser']['id']?></div> */ ?>
-  <?php print $contestants; ?>
+  <?php 
+  // foreach ( array(1 => 'winner', 2 => 'loser') as $k => $v ) {
+    // print "<div class=\"contestant tourney-contestant contestant-$k\">";
+    // print (isset($match['previousMatches'][$k]) ? $letters[$match['previousMatches'][$k]] : '@') . ' - ';
+    // print $letters[$match['id']] . ' - ';
+    // print (isset($match['nextMatch'][$v]['id']) ? $letters[$match['nextMatch'][$v]['id']] : '@');
+    // print (isset($match['nextMatch'][$v]['slot']) ? ' - ' . $match['nextMatch'][$v]['slot'] : '@');
+    // print isset($match['seeds']) && isset($match['seeds'][$k]) ? ' - ' . $match['seeds'][$k] : '';
+    // print "</div>";
+  // } ?>
+  <?php 
+    print $contestants; 
+  ?>
 </div>
