0fa5423a8641a7e2cb12ab6411621f87f6fc4ccf
dragonwize <drupal@dragonwize.net>
2012-09-24 14:40:04 -0700

    Removing trailing space. Adding line feeds at end of files.

--- plugins/tourney_formats/single_elim/theme/tourney-tournament-tree-node.tpl.php
+++ plugins/tourney_formats/single_elim/theme/tourney-tournament-tree-node.tpl.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @file tourney-tournament-tree-node.tpl.php
- * 
+ *
  * Creates the table based layout of each match and its children.  This template
  * is recursive and will create each match and it's children mark-up in the
  * children div of each iteration.
@@ -9,7 +9,6 @@
  * @ingroup tourney_templates
  */
 ?>
-
 <div class="tree-node <?php print $node_classes; ?>">
   <?php if ($has_children && count($node['children'])): ?>
     <div class="children">
