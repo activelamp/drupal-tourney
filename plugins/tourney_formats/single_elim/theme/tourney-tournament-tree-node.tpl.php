3a97b38f4fcefd54760c797e20ecaef328140710
Tom Friedhof <tom@activelamp.com>
2012-09-24 15:56:47 -0700

    removed the requirement to set a contestant on a match, put the bracket rendering back to template file for now

--- plugins/tourney_formats/single_elim/theme/tourney-tournament-tree-node.tpl.php
+++ plugins/tourney_formats/single_elim/theme/tourney-tournament-tree-node.tpl.php
@@ -23,10 +23,10 @@
       <h2 class="round-title"><?php print $round_name; ?></h2>
     <?php endif; ?>
     <?php 
-      $match_entity = entity_view('tourney_match', array($match->id => $match), 'match_block'); 
-      print drupal_render($match_entity);
+      // $match_entity = entity_view('tourney_match', array($match->id => $match), 'match_block'); 
+      // print drupal_render($match_entity);
     ?>
-    <?php //print theme('tourney_match_render', array('match' => $node, 'plugin' => $plugin)); ?>
+    <?php print theme('tourney_match_render', array('match' => $node, 'plugin' => $plugin)); ?>
     <div class="connector to-parent <?php print $node_classes; ?> <?php print $path_classes; ?>">
       <div class="path"></div>
     </div>
