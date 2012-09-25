faa77852944bd645b1a5428a7965c615bb3a3575
Jonathan Roberts <jon@activelamp.com>
2012-09-25 13:20:41 -0600

    match_block field for match_block view mode

--- plugins/tourney_formats/single_elim/theme/tourney-tournament-tree-node.tpl.php
+++ plugins/tourney_formats/single_elim/theme/tourney-tournament-tree-node.tpl.php
@@ -23,10 +23,9 @@
       <h2 class="round-title"><?php print $round_name; ?></h2>
     <?php endif; ?>
     <?php 
-      // $match_entity = entity_view('tourney_match', array($match->id => $match), 'match_block'); 
-      // print drupal_render($match_entity);
+      print drupal_render($match->view('match_block'));
     ?>
-    <?php print theme('tourney_match_render', array('match' => $node, 'plugin' => $plugin)); ?>
+    <?php //print theme('tourney_match_render', array('match' => $node, 'plugin' => $plugin)); ?>
     <div class="connector to-parent <?php print $node_classes; ?> <?php print $path_classes; ?>">
       <div class="path"></div>
     </div>
