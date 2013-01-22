55da0e7581ad2ffbcc29396418a93e96a1f438a4
Tom Friedhof <tom@activelamp.com>
2013-01-21 23:08:47 -0800

    moved the batch operation to the tourney module, removed a couple php notices

--- plugins/tourney_formats/BaseFormatControllers/ManualUpload.php
+++ plugins/tourney_formats/BaseFormatControllers/ManualUpload.php
@@ -354,7 +354,7 @@ class ManualUploadController extends TourneyController {
     // Build our data structure
     $this->build();
     $this->structure();
-    drupal_add_js($this->pluginInfo['path'] . '/theme/manualupload_match_times_validate.js');
+    // drupal_add_js($this->pluginInfo['path'] . '/theme/manualupload_match_times_validate.js');
     return theme('tourney_tournament_render', array('plugin' => $this));
   }
 
